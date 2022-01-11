43e4ab3997674b7f3f0f002187a56bbfcf11b88d
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-12 01:28:17 +0300

    add project class

--- portapp/models.py
+++ portapp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    image = CloudinaryField("image")
+    description = models.TextField(max_length=600)
+    technologies = models.TextField(max_length=100, null=True)
+    url = models.URLField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    
+    @classmethod
+    def search_project_name(cls, search_term):
+        projects = cls.objects.filter(
+        title__icontains=search_term)
+        return projects    
+
+    def str(self):
+        return self.user.username
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_all_projects_by_user(cls, user):
+        projects = cls.objects.filter(user=user)
+        return projects
+
+    def update_project(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
