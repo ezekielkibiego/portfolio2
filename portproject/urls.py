85743d0667f7a1be75cbbfcf9acdb43ddda988ff
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-12 01:27:36 +0300

    add login path

--- portproject/urls.py
+++ portproject/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('',include('portapp.urls')),
     # path('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
     # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+   
     # path('logout/', views.logout_then_login, name='logout'), 
     
     
