075eaa9fe56f20da7770fa2fc8009ba8577e3048
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-16 04:40:28 +0300

    adding about path

--- portapp/urls.py
+++ portapp/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('',views.index ,name = 'index'),
     path('account/login/',views.user_login ,name = 'login'),
     path('projects/',views.projects ,name = 'projects'),
-    # path('create_comment',views.create_comment ,name = 'create_comment'),
+    path('contact/',views.contact ,name = 'contact'),
+    path('github',views.github ,name = 'github'),
+    path('about/',views.about ,name = 'about'),
     
     
     
